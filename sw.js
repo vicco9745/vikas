03a3d30118f6fd8b326af000548f08e995865453
vicco9745 <vicco9745@gmail.com>
2026-01-25 22:33:01 +0530

    Refactor service worker event listeners and cache

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
-self.addEventListener("install", function(e) {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("calculator-cache").then(function(cache) {
-      return cache.addAll(["./"]);
+    caches.open("calculator-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon.png"
+      ]);
     })
   );
 });
 
-self.addEventListener("fetch", function(e) {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(function(res) {
-      return res || fetch(e.request);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
